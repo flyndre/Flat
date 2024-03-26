474cf994f36e9492cec2416c9a9bb59aca3776d1
dasdawidt <David_Maier@gmx.de>
2024-03-26 23:21:51 +0100

    feat: add map view

--- frontend-pwa/src/router/index.ts
+++ frontend-pwa/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "@/views/HomeView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import MapView from "@/views/MapView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,11 @@ const router = createRouter({
             path: "/",
             name: "home",
             component: HomeView,
+        },
+        {
+            path: "/map",
+            name: "map",
+            component: MapView,
         },
         {
             path: "/:pathMatch(.*)*",
