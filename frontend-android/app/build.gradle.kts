ceb7c1d9776657daa95746f9777ac835420d08c8
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-03-27 12:40:18 +0100

    added gradle plugin for using api keys

--- frontend-android/app/build.gradle.kts
+++ frontend-android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
