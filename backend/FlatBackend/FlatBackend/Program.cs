c597f6ee8c48d365a92e6c70473e33c2a4586711
Yosgarat <106805499+Yosgarat@users.noreply.github.com>
2024-03-28 17:43:06 +0100

    First try of websocket implementation

--- backend/FlatBackend/FlatBackend/Program.cs
+++ backend/FlatBackend/FlatBackend/Program.cs
@@ -1,6 +1,37 @@
+using System.Diagnostics.Eventing.Reader;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("https://localhost:44380/");
 var app = builder.Build();
+app.UseWebSockets();
+app.MapGet("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        using var ws = await context.WebSockets.AcceptWebSocketAsync();
+        while (true)
+        {
+            var message = "The current time is : " + DateTime.Now.ToString("HH:MM:SS");
+            var bytes = Encoding.UTF8.GetBytes(message);
+            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+            if (ws.State == WebSocketState.Open)
+            {
+                await ws.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+            else if (ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
+            {
+                break;
+            }
+            Thread.Sleep(1000);
+        }
+    }
+    else
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+    }
+});
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+await app.RunAsync();
