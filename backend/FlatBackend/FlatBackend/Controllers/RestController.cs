1b1193e32b045a209e11a42c6a1c5b496a7ac755
Yosgarat <106805499+Yosgarat@users.noreply.github.com>
2024-04-05 11:03:00 +0200

    Rest-Api endpoints added

--- backend/FlatBackend/FlatBackend/Controllers/RestController.cs
+++ backend/FlatBackend/FlatBackend/Controllers/RestController.cs
@@ -9,35 +9,43 @@ namespace FlatBackend.Controllers
     public class RestController : ControllerBase
     {
         // GET: api/<ValuesController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
-        // GET api/<ValuesController>/5
-        [HttpGet("{id}")]
-        public string Get( int id )
-        {
-            return "value";
-        }
+        //// GET api/<ValuesController>/5
+        //[HttpGet("{id}")]
+        //public string Get( int id )
+        //{
+        //    return "value";
+        //}
 
         // POST api/<ValuesController>
+        //[HttpPost]
+        //public void Post( [FromBody] string value )
+        //{
+        //}
+
         [HttpPost]
-        public void Post( [FromBody] string value )
+        public void PostOpenCollection( [FromBody] string value )
         {
+            //call create Database and send Objekt back
         }
 
         // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public void Put( int id, [FromBody] string value )
+        [HttpPut("Collection/{id}")]
+        public void PutSetOrChangeAreaDivision( int id, [FromBody] string value )
         {
+            //call Change or set AreaDivision in Database
         }
 
-        // DELETE api/<ValuesController>/5
-        [HttpDelete("{id}")]
-        public void Delete( int id )
+        // DELETE api/<ValuesController>/5 CloseCollection
+        [HttpDelete("Collection/{id}")]
+        public void DeleteCollection( int id )
         {
+            //call delete Collection in Database
         }
     }
 }
