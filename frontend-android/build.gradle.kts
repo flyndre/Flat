5d46314fa1d60088ca9eaf9a978e31faadb23d2e
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-04-05 10:05:28 +0200

    added dependencies for android room database

--- frontend-android/build.gradle.kts
+++ frontend-android/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
 }
 
 buildscript {
