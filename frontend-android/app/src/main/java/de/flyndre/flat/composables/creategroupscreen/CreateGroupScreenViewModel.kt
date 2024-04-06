04ba234a2e58abf362114f2136f17bbc68cfa394
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-04-06 10:59:19 +0200

    added add-button for creating new preset

--- frontend-android/app/src/main/java/de/flyndre/flat/composables/creategroupscreen/CreateGroupScreenViewModel.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/composables/creategroupscreen/CreateGroupScreenViewModel.kt
@@ -1,13 +1,18 @@
 package de.flyndre.flat.composables.creategroupscreen
 
+import android.util.Log
+import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
 import androidx.room.Room
 import de.flyndre.flat.database.AppDatabase
+import de.flyndre.flat.database.entities.Preset
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.transform
 
-class CreateGroupScreenViewModel: ViewModel() {
+class CreateGroupScreenViewModel(db: AppDatabase) : ViewModel() {
     private val _createGroupState = MutableStateFlow(CreateGroupScreenState())
     val createGroupState: StateFlow<CreateGroupScreenState> = _createGroupState.asStateFlow()
 }
