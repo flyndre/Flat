ed7f41378c4b18bfbfb24b83a4ede70c0c52108a
dasdawidt <David_Maier@gmx.de>
2024-04-08 22:19:57 +0200

    feat: add main wrapper component

---
+++ frontend-pwa/src/components/MainWrapper.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { isOnMobile } from "@/util/mobileDetection";
+</script>
+
+<template>
+    <main
+        class="flex flex-col gap-2 justify-start items-stretch"
+        :class="{ 'px-2': isOnMobile }"
+    >
+        <slot />
+    </main>
+</template>
