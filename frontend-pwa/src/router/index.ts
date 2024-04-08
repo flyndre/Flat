e1348f3490edb06bf66c49b6ba54f39e716759e9
dasdawidt <David_Maier@gmx.de>
2024-04-08 23:37:57 +0200

    feat: add about page

--- frontend-pwa/src/router/index.ts
+++ frontend-pwa/src/router/index.ts
@@ -6,6 +6,7 @@ import MapView from "@/views/MapView.vue";
 import PresetsView from "@/views/PresetsView.vue";
 import EditView from "@/views/EditView.vue";
 import JoinView from "@/views/JoinView.vue";
+import AboutView from "@/views/AboutView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
             path: "/",
             name: "home",
             component: HomeView,
+        },
+        {
+            path: "/about",
+            name: "about",
+            component: AboutView,
         },
         {
             path: "/join",
