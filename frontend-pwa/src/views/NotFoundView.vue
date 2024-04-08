d85f92c108b5d3e9d9481ec9c71be9bd3a604961
dasdawidt <David_Maier@gmx.de>
2024-04-09 00:27:41 +0200

    feat: update 404 page

--- frontend-pwa/src/views/NotFoundView.vue
+++ frontend-pwa/src/views/NotFoundView.vue
@@ -1,16 +1,37 @@
 <script setup lang="ts">
+import MdiIcon from "@/components/MdiIcon.vue";
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import { mdiArrowLeft, mdiHome } from "@mdi/js";
 import Button from "primevue/button";
 import Card from "primevue/card";
+import { useRouter } from "vue-router";
+const router = useRouter();
 </script>
 
 <template>
-    <Card>
-        <template #title> 404 </template>
-        <template #subtitle>Page not found</template>
-        <template #footer>
+    <DefaultLayout>
+        <template #action-left>
+            <Button label="Back" severity="secondary" text @click="router.back">
+                <template #icon>
+                    <MdiIcon class="mr-2.5" :icon="mdiArrowLeft" />
+                </template>
+            </Button>
+        </template>
+        <template #title> Error </template>
+        <template #action-right>
             <router-link :to="{ name: 'home' }">
-                <Button label="Go Home" />
+                <Button label="Go Home">
+                    <template #icon>
+                        <MdiIcon class="mr-2.5" :icon="mdiHome" />
+                    </template>
+                </Button>
             </router-link>
         </template>
-    </Card>
+        <template #default>
+            <Card>
+                <template #title> 404 </template>
+                <template #subtitle>Page not found</template>
+            </Card>
+        </template>
+    </DefaultLayout>
 </template>
