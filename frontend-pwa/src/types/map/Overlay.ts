eed4a8f154fc7c2ffd2afbbce9e04c6b830b2c84
dasdawidt <David_Maier@gmx.de>
2024-04-13 17:15:46 +0200

    fix: make type optional

--- frontend-pwa/src/types/map/Overlay.ts
+++ frontend-pwa/src/types/map/Overlay.ts
@@ -1,5 +1,6 @@
-export type Overlay =
+export type Overlay = { type?: google.maps.drawing.OverlayType } & (
     | google.maps.Polygon
     | google.maps.Polyline
     | google.maps.Rectangle
-    | google.maps.Circle;
+    | google.maps.Circle
+);
