8e26fdad5cf7651cc62a81c879723caf18e47025
Yosgarat <106805499+Yosgarat@users.noreply.github.com>
2024-04-13 13:45:02 +0200

    deleted commented code in Program.cs

--- backend/FlatBackend/FlatBackend/Program.cs
+++ backend/FlatBackend/FlatBackend/Program.cs
@@ -23,32 +23,6 @@ var webSocketOptions = new WebSocketOptions
     KeepAliveInterval = TimeSpan.FromMinutes(2)
 };
 app.UseWebSockets(webSocketOptions);
-/* app.MapGet("/ws", async context =>
-{
-    if (context.WebSockets.IsWebSocketRequest)
-    {
-        using var ws = await context.WebSockets.AcceptWebSocketAsync();
-        while (true)
-        {
-            var message = "The current time is : " + DateTime.Now.ToString("HH:MM:SS");
-            var bytes = Encoding.UTF8.GetBytes(message);
-            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
-            if (ws.State == WebSocketState.Open)
-            {
-                await ws.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
-            }
-            else if (ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
-            {
-                break;
-            }
-            Thread.Sleep(1000);
-        }
-    }
-    else
-    {
-        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-    }
-});*/
 
 app.UseHttpsRedirection();
 
