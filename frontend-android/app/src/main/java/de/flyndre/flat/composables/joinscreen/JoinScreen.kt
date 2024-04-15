0492df96eb44ac2023faa79dcd0966f08a070c9b
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-04-15 10:49:09 +0200

    implemented basic layout for join screen

--- frontend-android/app/src/main/java/de/flyndre/flat/composables/joinscreen/JoinScreen.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/composables/joinscreen/JoinScreen.kt
@@ -1,20 +1,28 @@
 package de.flyndre.flat.composables.joinscreen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun JoinScreen(modifier: Modifier = Modifier, onNavigateToInitialScreen: () -> Unit, joinScreenViewModel: JoinScreenViewModel) {
+    val joinLink by joinScreenViewModel.joinLink.collectAsState()
+    val joinName by joinScreenViewModel.joinName.collectAsState()
     Scaffold(
         topBar = {
             TopAppBar(title = { Text(text = "") }, navigationIcon = {
@@ -26,6 +34,13 @@ fun JoinScreen(modifier: Modifier = Modifier, onNavigateToInitialScreen: () -> U
                 }
             })
         }) { innerPadding ->
-        modifier.padding(innerPadding)
+        Column(modifier = modifier.padding(innerPadding)) {
+            val modifier = Modifier.padding(10.dp)
+            TextField(modifier = modifier, value = joinLink, onValueChange = {joinScreenViewModel.updateJoinLink(it)}, label = {Text(text = "Link for joining")})
+            TextField(modifier = modifier, value = joinName, onValueChange = {joinScreenViewModel.updateJoinName(it)}, label = {Text(text = "Name for joining")})
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Join")
+            }
+        }
     }
 }
