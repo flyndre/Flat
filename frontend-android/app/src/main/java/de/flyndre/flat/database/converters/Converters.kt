0cc539e7af8d072d54119bfa3c7b6c588140f3d3
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-04-16 14:20:19 +0200

    added room converters for CameraPosition

--- frontend-android/app/src/main/java/de/flyndre/flat/database/converters/Converters.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/database/converters/Converters.kt
@@ -1,6 +1,7 @@
 package de.flyndre.flat.database.converters
 
 import androidx.room.TypeConverter
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -19,5 +20,19 @@ class Converters {
         val gson: Gson = Gson()
         val list = gson.fromJson<ArrayList<LatLng>>(json, object: TypeToken<ArrayList<LatLng>>(){}.type)
         return list
+    }
+
+    @TypeConverter
+    fun fromCameraPosition(cameraPosition: CameraPosition): String{
+        val gson: Gson = Gson()
+        val json: String = gson.toJson(cameraPosition, object: TypeToken<CameraPosition>(){}.type)
+        return json
+    }
+
+    @TypeConverter
+    fun toCameraPosition(json: String): CameraPosition{
+        val gson: Gson = Gson()
+        val cameraPosition = gson.fromJson<CameraPosition>(json, object: TypeToken<CameraPosition>(){}.type)
+        return cameraPosition
     }
 }
