185283656acc41cb7f41409a2b791b8b39a2a739
L4kiLuk <49391930+L4kiLuk@users.noreply.github.com>
2024-04-19 10:37:02 +0200

    add real LocationService fix issue with single point track conversion to linestring

--- frontend-android/app/src/main/java/de/flyndre/flat/models/Track.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/models/Track.kt
@@ -1,7 +1,6 @@
 package de.flyndre.flat.models
 
 import io.github.dellisd.spatialk.geojson.LineString
-import io.github.dellisd.spatialk.geojson.Point
 import io.github.dellisd.spatialk.geojson.Position
 import java.util.UUID
 
@@ -9,9 +8,11 @@ class Track(var trackId: UUID = UUID.randomUUID()): ArrayList<Position>() {
 
     fun toLineString():LineString{
         if(size<2){
-            return LineString(Position(0.0,0.0),Position(0.0,0.0))
+            return LineString(listOf(this.last(),this.last()))
         }
         return LineString(this)
     }
+
+
 
 }
--- frontend-android/app/src/main/java/de/flyndre/flat/services/RandomLocationService.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/services/RandomLocationService.kt
@@ -2,38 +2,46 @@ package de.flyndre.flat.services
 
 import android.util.Log
 import de.flyndre.flat.interfaces.ILocationService
-import de.flyndre.flat.interfaces.ITrackingService
 import io.github.dellisd.spatialk.geojson.Position
 import java.util.concurrent.ThreadLocalRandom
-import kotlin.random.Random
 
-class RandomLocationService(override var trackingService: ITrackingService, var interval:Long = 5000) : ILocationService {
-    var lat1 = 48.414400
-    var lon1 =  8.490597
-    var lat2 = 48.424255
-    var lon2 = 8.506647
-    var lastlat = ThreadLocalRandom.current().nextDouble(lat1,lat2)
-    var lastlon = ThreadLocalRandom.current().nextDouble(lon1,lon2)
-    lateinit var thread :Thread
+class RandomLocationService(
+    override val updateInterval: Long = 5000,
+    override val onLocationUpdate: ArrayList<(Position) -> Unit> = arrayListOf()
+) : ILocationService {
+    override var isTracking: Boolean = false
+    private var lat1 = 48.414400
+    private var lon1 =  8.490597
+    private var lat2 = 48.424255
+    private var lon2 = 8.506647
+    private var lastlat = ThreadLocalRandom.current().nextDouble(lat1,lat2)
+    private var lastlon = ThreadLocalRandom.current().nextDouble(lon1,lon2)
+    private lateinit var thread :Thread
 
     override fun startTracking() {
         try {
-            thread = Thread(Runnable {
-                try{
-                    while(!Thread.interrupted()) {
-                        lastlon = ThreadLocalRandom.current().nextDouble(lastlon-0.0001,lastlon+0.0001)
-                        lastlat = ThreadLocalRandom.current().nextDouble(lastlat-0.0001,lastlat+0.0001)
-                        trackingService.addNewPosition(
-                            Position(
-                                lastlon,
-                                lastlat
+            thread = Thread {
+                try {
+                    while (!Thread.interrupted()) {
+                        lastlon = ThreadLocalRandom.current()
+                            .nextDouble(lastlon - 0.0001, lastlon + 0.0001)
+                        lastlat = ThreadLocalRandom.current()
+                            .nextDouble(lastlat - 0.0001, lastlat + 0.0001)
+                        onLocationUpdate.stream().forEach { x ->
+                            x(
+                                Position(
+                                    lastlon,
+                                    lastlat
+                                )
                             )
-                        )
-                        Thread.sleep(interval)
+                        }
+                        Thread.sleep(updateInterval)
                     }
-                }catch (_:InterruptedException){}
-            })
+                } catch (_: InterruptedException) {
+                }
+            }
             thread.start()
+            isTracking = true
         }catch (ex:Exception){
             Log.e(this.toString(),ex.toString())
         }
@@ -42,5 +50,10 @@ class RandomLocationService(override var trackingService: ITrackingService, var 
 
     override fun stopTracking() {
         thread.interrupt()
+        isTracking=false
+    }
+
+    override fun addOnLocationUpdate(callback: (Position) -> Unit) {
+        onLocationUpdate.add(callback)
     }
 }
