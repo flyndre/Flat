884233c59927685ea7b83ff494635d025e1baa55
Yosgarat <106805499+Yosgarat@users.noreply.github.com>
2024-04-22 11:32:18 +0200

    Added TrackModel

---
+++ backend/FlatBackend/FlatBackend/Models/TrackModel.cs
@@ -0,0 +1,11 @@
+﻿using FlatBackend.Models.GeoJsonModels;
+
+namespace FlatBackend.Models
+{
+    public class TrackModel
+    {
+        public Guid trackId { get; set; }
+        public Guid? clientId { get; set; }
+        public LineString incrementalTrack { get; set; }
+    }
+}
