736de71c4490c18ce7dc4b7ac9f75cab113721ca
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-04-22 11:56:37 +0200

    changed Preset entity to use collection area data class

--- frontend-android/app/src/main/java/de/flyndre/flat/database/entities/Preset.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/database/entities/Preset.kt
@@ -5,12 +5,13 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import de.flyndre.flat.composables.presetscreen.collectionareascreen.CollectionArea
 
 @Entity
 data class Preset(
     @PrimaryKey(autoGenerate = true) val id: Long,
     @ColumnInfo(name = "presetName") val presetName: String,
     @ColumnInfo(name = "presetDescription") val presetDescription: String,
-    @ColumnInfo(name = "presetAreaPoints") val presetAreaPoints: ArrayList<LatLng>,
+    @ColumnInfo(name = "presetCollectionAreas") val presetCollectionAreas: ArrayList<CollectionArea>,
     @ColumnInfo(name = "presetCameraPosition") val presetCameraPosition: CameraPosition
 )
