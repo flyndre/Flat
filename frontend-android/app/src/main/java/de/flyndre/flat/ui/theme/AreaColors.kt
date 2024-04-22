ece3beb83758fdfefff76ba4a69fe4208e09ad72
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-04-22 11:00:01 +0200

    added Area Colors

---
+++ frontend-android/app/src/main/java/de/flyndre/flat/ui/theme/AreaColors.kt
@@ -0,0 +1,9 @@
+package de.flyndre.flat.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue = Color(0xFF1E90FF);
+val Pink = Color(0xFFFF1493)
+val Green = Color(0xFF32CD32)
+val Orange = Color(0xFFFF8C00)
+val Purple = Color(0xFF4B0082)
