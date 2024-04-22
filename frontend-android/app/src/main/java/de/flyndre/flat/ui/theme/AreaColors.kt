bd9fd97bc3a4c8a0dd1441ab1413df48a7a81701
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-04-22 11:14:11 +0200

    renamed colors for collection areas

--- frontend-android/app/src/main/java/de/flyndre/flat/ui/theme/AreaColors.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/ui/theme/AreaColors.kt
@@ -2,8 +2,8 @@ package de.flyndre.flat.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Blue = Color(0xFF1E90FF);
-val Pink = Color(0xFFFF1493)
-val Green = Color(0xFF32CD32)
-val Orange = Color(0xFFFF8C00)
-val Purple = Color(0xFF4B0082)
+val AreaBlue = Color(0xFF1E90FF);
+val AreaPink = Color(0xFFFF1493)
+val AreaGreen = Color(0xFF32CD32)
+val AreaOrange = Color(0xFFFF8C00)
+val AreaPurple = Color(0xFF4B0082)
