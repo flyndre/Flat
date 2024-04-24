b7c64ecda82e1f1e6d60236488fce7a5427d71c4
dasdawidt <David_Maier@gmx.de>
2024-04-24 11:03:04 +0200

    fix: shapes selectable on touch screen

--- frontend-pwa/src/components/map/MapWithControls.vue
+++ frontend-pwa/src/components/map/MapWithControls.vue
@@ -446,6 +446,7 @@ const lineOptions = {
         var newShape = typedOverlay.overlay;
         newShape.type = typedOverlay.type;
         google.maps.event.addListener(newShape, 'click', function () {
+            if (props.locked) return;
             setSelection(newShape);
         });
         if (userCreated) {
