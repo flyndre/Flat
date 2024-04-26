0423e741d36025312502b97eba4618fdfc5b8d16
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-04-26 12:25:51 +0200

    implemented composable function for admin menu

--- frontend-android/app/src/main/java/de/flyndre/flat/composables/trackingscreen/TrackingScreen.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/composables/trackingscreen/TrackingScreen.kt
@@ -1,16 +1,21 @@
 package de.flyndre.flat.composables.trackingscreen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -26,6 +31,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import de.flyndre.flat.models.Track
+import kotlin.math.exp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -74,6 +80,22 @@ fun TrackingScreen(modifier: Modifier = Modifier, trackingScreenViewModel: Track
                     Text(text = track.toLineString().toString())
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun AdminMenu(){
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier){
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            DropdownMenuItem(text = { Text(text = "End Collection") }, onClick = { /*TODO*/ })
+            HorizontalDivider()
+            DropdownMenuItem(text = { Text(text = "Manage Groups") }, onClick = { /*TODO*/ })
+        }
+        IconButton(onClick = { expanded = true }) {
+            Icon(Icons.Filled.MoreVert, contentDescription = "open collection management")
         }
     }
 }
