87e54fc9a40ad469f70342f645939a17c7d91e32
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-04-26 09:49:48 +0200

    added function for calculating bounds of collectionarea

--- frontend-android/app/src/main/java/de/flyndre/flat/composables/presetscreen/collectionareascreen/CollectionArea.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/composables/presetscreen/collectionareascreen/CollectionArea.kt
@@ -2,5 +2,31 @@ package de.flyndre.flat.composables.presetscreen.collectionareascreen
 
 import androidx.compose.ui.graphics.Color
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 
 data class CollectionArea(val color: Color, val isSelected: Boolean, val listAreaPoints: ArrayList<LatLng>)
+
+fun generateBounds(collectionArea: CollectionArea): LatLngBounds {
+    var north: Double = collectionArea.listAreaPoints.get(0).latitude
+    var east: Double = collectionArea.listAreaPoints.get(0).longitude
+    var south: Double = collectionArea.listAreaPoints.get(0).latitude
+    var west: Double = collectionArea.listAreaPoints.get(0).longitude
+
+    for(point in collectionArea.listAreaPoints){
+        if(point.longitude > north){
+            north = point.longitude
+        }
+        if(point.longitude < south){
+            south = point.longitude
+        }
+        if(point.latitude > east){
+            east = point.latitude
+        }
+        if(point.latitude < west){
+            west = point.latitude
+        }
+    }
+
+    val bounds = LatLngBounds(LatLng(west, south), LatLng(east, north))
+    return bounds
+}
