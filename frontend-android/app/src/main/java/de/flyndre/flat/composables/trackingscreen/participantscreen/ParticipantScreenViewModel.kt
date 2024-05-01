c7f910ba0ed3cf3f8f714ea59c9deba1c3c29c84
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-05-01 21:53:15 +0200

    added list of users to participantscreenviewmodel

--- frontend-android/app/src/main/java/de/flyndre/flat/composables/trackingscreen/participantscreen/ParticipantScreenViewModel.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/composables/trackingscreen/participantscreen/ParticipantScreenViewModel.kt
@@ -1,6 +1,16 @@
 package de.flyndre.flat.composables.trackingscreen.participantscreen
 
 import androidx.lifecycle.ViewModel
+import de.flyndre.flat.models.UserModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class ParticipantScreenViewModel: ViewModel() {
+    private val _users = MutableStateFlow(arrayListOf<UserModel>())
+    val users = _users.asStateFlow()
+
+    fun setUsers(users: List<UserModel>){
+        _users.value = arrayListOf()
+        _users.value.addAll(users)
+    }
 }
