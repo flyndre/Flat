f97dbcf31de1d4006823f0b48266191a3c615e0a
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-05-02 10:03:44 +0200

    added list of divisions to participantscreen

--- frontend-android/app/src/main/java/de/flyndre/flat/composables/trackingscreen/participantscreen/ParticipantScreen.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/composables/trackingscreen/participantscreen/ParticipantScreen.kt
@@ -1,20 +1,32 @@
 package de.flyndre.flat.composables.trackingscreen.participantscreen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ParticipantScreen(participantScreenViewModel: ParticipantScreenViewModel, onNavigateToTrackingScreen: () -> Unit,){
+    val divisions by participantScreenViewModel.divisions.collectAsState()
+    var expanded by remember { mutableStateOf(false) }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -30,6 +42,15 @@ fun ParticipantScreen(participantScreenViewModel: ParticipantScreenViewModel, on
         }
     ) {
         innerPadding ->
-        Modifier.padding(innerPadding)
+        LazyColumn(Modifier.padding(innerPadding)) {
+            items(divisions){
+                ListItem(headlineContent = { Text(text = it.name) }, trailingContent = { ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = {expanded != expanded}
+                ) {
+
+                }})
+            }
+        }
     }
 }
