ddb1e002e69a6494ebc470361f22d3d490e7131f
Yosgarat <106805499+Yosgarat@users.noreply.github.com>
2024-05-03 16:41:50 +0200

    Added periodical KeepAliveMessage

---
+++ backend/FlatBackend/FlatBackend/DTOs/KeepAliveDto.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace FlatBackend.DTOs
+{
+    public class KeepAliveDto
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public WebSocketMessageType type { get; set; } = WebSocketMessageType.KeepAlive;
+    }
+}
