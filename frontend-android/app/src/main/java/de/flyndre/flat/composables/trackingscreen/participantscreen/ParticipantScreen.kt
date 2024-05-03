4dad258e3b60530a11f6620a11cb6218a8a6f7f7
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-05-03 16:57:10 +0200

    implemented ui for setting user of collection area

--- frontend-android/app/src/main/java/de/flyndre/flat/composables/trackingscreen/participantscreen/ParticipantScreen.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/composables/trackingscreen/participantscreen/ParticipantScreen.kt
@@ -1,18 +1,20 @@
 package de.flyndre.flat.composables.trackingscreen.participantscreen
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -26,14 +28,17 @@ import de.flyndre.flat.models.UserModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ParticipantScreen(participantScreenViewModel: ParticipantScreenViewModel, onNavigateToTrackingScreen: () -> Unit,){
+fun ParticipantScreen(
+    participantScreenViewModel: ParticipantScreenViewModel,
+    onNavigateToTrackingScreen: () -> Unit,
+) {
     val divisions by participantScreenViewModel.divisions.collectAsState()
     val users by participantScreenViewModel.users.collectAsState()
 
     Scaffold(
         topBar = {
             TopAppBar(
-                title = {  },
+                title = { },
                 navigationIcon = {
                     IconButton(onClick = { onNavigateToTrackingScreen() }) {
                         Icon(
@@ -43,11 +48,18 @@ fun ParticipantScreen(participantScreenViewModel: ParticipantScreenViewModel, on
                     }
                 })
         }
-    ) {
-        innerPadding ->
+    ) { innerPadding ->
         LazyColumn(Modifier.padding(innerPadding)) {
-            items(divisions){
-                ListItem(headlineContent = { Text(text = it.name) }, trailingContent = { DropDownUserSelection(participantScreenViewModel, it, users)})
+            items(divisions) {
+                ListItem(
+                    headlineContent = { Text(text = it.name) },
+                    trailingContent = {
+                        DropDownUserSelection(
+                            participantScreenViewModel,
+                            it,
+                            users
+                        )
+                    })
             }
         }
     }
@@ -55,10 +67,41 @@ fun ParticipantScreen(participantScreenViewModel: ParticipantScreenViewModel, on
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DropDownUserSelection(participantScreenViewModel: ParticipantScreenViewModel, division: CollectionArea, users: List<UserModel>){
+fun DropDownUserSelection(
+    participantScreenViewModel: ParticipantScreenViewModel,
+    division: CollectionArea,
+    users: List<UserModel>,
+) {
     var expanded by remember { mutableStateOf(false) }
-    var selectedUser: UserModel? by remember { mutableStateOf(users.find { it.clientId.equals(division.clientId) }) }
-    ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = {expanded = it}) {
-        
+    var selectedUser: UserModel? by remember {
+        mutableStateOf(users.find {
+            it.clientId.equals(
+                division.clientId
+            )
+        })
+    }
+    var userDisplay: String = ""
+    if (selectedUser != null) {
+        userDisplay = selectedUser!!.username
+    }
+    ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = { expanded = it }) {
+        TextField(
+            value = userDisplay,
+            onValueChange = {},
+            modifier = Modifier.menuAnchor(),
+            readOnly = true,
+            label = {
+                Text(
+                    text = "Benutzer"
+                )
+            },
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            colors = ExposedDropdownMenuDefaults.textFieldColors()
+        )
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            users.forEach{user ->
+                DropdownMenuItem(text = { Text(text = user.username) }, onClick = { participantScreenViewModel.setUserOfDivision(division, user) })
+            }
+        }
     }
 }
