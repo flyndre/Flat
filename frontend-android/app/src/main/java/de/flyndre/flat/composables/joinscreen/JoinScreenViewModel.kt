e8027585617fc8d433905794716c0e305db055a3
L4kiLuk <49391930+L4kiLuk@users.noreply.github.com>
2024-05-13 14:34:46 +0200

    add name and collection history

--- frontend-android/app/src/main/java/de/flyndre/flat/composables/joinscreen/JoinScreenViewModel.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/composables/joinscreen/JoinScreenViewModel.kt
@@ -1,6 +1,9 @@
 package de.flyndre.flat.composables.joinscreen
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.util.Log
+import androidx.core.content.edit
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -14,13 +17,17 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import java.lang.IllegalArgumentException
 import java.util.UUID
+import java.util.prefs.Preferences
 
-class JoinScreenViewModel(db: AppDatabase,trackingScreenViewModel: TrackingScreenViewModel, connectionService: IConnectionService): ViewModel() {
+class JoinScreenViewModel(db: AppDatabase,preferences:SharedPreferences,trackingScreenViewModel: TrackingScreenViewModel, connectionService: IConnectionService): ViewModel() {
     //appdatabase
     private val _db = db
     private val _trackingScreenViewModel = trackingScreenViewModel
     //connection service
     private val _connectionService = connectionService
+    private val _preferences = preferences
+    private val _usernameKey = "USERNAME"
+    private val _lastCollectionsKey = "LASTCOLLECTIONS"
     //join link
     private val _joinLink = MutableStateFlow("")
     val joinLink: StateFlow<String> = _joinLink.asStateFlow()
@@ -30,11 +37,28 @@ class JoinScreenViewModel(db: AppDatabase,trackingScreenViewModel: TrackingScree
     }
 
     //join name
-    private val _joinName = MutableStateFlow("")
+    private val _joinName = MutableStateFlow(preferences.getString(_usernameKey,"").toString())
     val joinName: StateFlow<String> = _joinName.asStateFlow()
 
     fun updateJoinName(joinName: String){
         _joinName.value = joinName
+        _preferences.edit{
+            putString(_usernameKey,joinName)
+        }
+    }
+    private val _lastCollections = MutableStateFlow(preferences.getStringSet(_lastCollectionsKey,
+        setOf())!!)
+    val lastCollections = _lastCollections.asStateFlow()
+    fun updateLastCollections(lastCollection:String){
+        var newSet = _lastCollections.value
+        if(newSet.size>=5){
+            newSet = newSet-newSet.first()
+        }
+        newSet = newSet+lastCollection
+        _lastCollections.value = newSet
+        _preferences.edit {
+            putStringSet(_lastCollectionsKey, newSet)
+        }
     }
 
     fun join(navigateToTrackingScreen: ()->Unit){
@@ -45,6 +69,7 @@ class JoinScreenViewModel(db: AppDatabase,trackingScreenViewModel: TrackingScree
                 if(answer.accepted){
                     _trackingScreenViewModel.collectionInstance= answer.collection!!
                     _connectionService.openWebsocket(answer.collection.id!!)
+                    updateLastCollections(joinLink.value)
                     navigateToTrackingScreen()
                 }else{
                     //handle deny
@@ -57,9 +82,10 @@ class JoinScreenViewModel(db: AppDatabase,trackingScreenViewModel: TrackingScree
 }
 class JoinScreenViewModelFactory(
     val db: AppDatabase,
+    val preferences: SharedPreferences,
     val trackingScreenViewModel: TrackingScreenViewModel,
     val connectionService: IConnectionService) : ViewModelProvider.Factory{
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return JoinScreenViewModel(db,trackingScreenViewModel,connectionService) as T
+        return JoinScreenViewModel(db,preferences,trackingScreenViewModel,connectionService) as T
     }
 }
