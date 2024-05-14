9a6fda5eba0792f022037ff619c6df3b8aa88149
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-05-14 21:27:40 +0200

    added connection error message

--- frontend-android/app/src/main/java/de/flyndre/flat/composables/joinscreen/JoinScreen.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/composables/joinscreen/JoinScreen.kt
@@ -1,10 +1,13 @@
 package de.flyndre.flat.composables.joinscreen
 
+import android.app.AlertDialog
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -12,6 +15,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -26,14 +30,25 @@ import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun JoinScreen(modifier: Modifier = Modifier, onNavigateToInitialScreen: () -> Unit, onNavigateToTrackingScreen: () -> Unit, joinScreenViewModel: JoinScreenViewModel) {
+fun JoinScreen(
+    modifier: Modifier = Modifier,
+    onNavigateToInitialScreen: () -> Unit,
+    onNavigateToTrackingScreen: () -> Unit,
+    joinScreenViewModel: JoinScreenViewModel,
+) {
     val joinLink by joinScreenViewModel.joinLink.collectAsState()
     val joinName by joinScreenViewModel.joinName.collectAsState()
     val lastCollections by joinScreenViewModel.lastCollections.collectAsState()
-    //error handling
+    //input error handling
     var isLinkEmpty by remember { mutableStateOf(false) }
     var isNameEmpty by remember { mutableStateOf(false) }
-    
+    //connection error handling
+    val showConnectionError by joinScreenViewModel.showConnectionError.collectAsState()
+
+    if(showConnectionError){
+        ConnectionErrorDialog(onDecline = {joinScreenViewModel.hideConnectionError()})
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(title = { Text(text = "") }, navigationIcon = {
@@ -45,20 +60,39 @@ fun JoinScreen(modifier: Modifier = Modifier, onNavigateToInitialScreen: () -> U
                 }
             })
         }) { innerPadding ->
-        Column(modifier = modifier.padding(innerPadding),
-            horizontalAlignment = Alignment.Start) {
+        Column(
+            modifier = modifier.padding(innerPadding),
+            horizontalAlignment = Alignment.Start
+        ) {
             val modifier = Modifier.padding(10.dp)
-            TextField(modifier = modifier, value = joinLink, onValueChange = {joinScreenViewModel.updateJoinLink(it)}, label = {Text(text = "Link for joining")}, isError = isLinkEmpty, supportingText = {if(isLinkEmpty){
-                Text(text = "Darf nicht leer sein")} })
-            TextField(modifier = modifier, value = joinName, onValueChange = {joinScreenViewModel.updateJoinName(it)}, label = {Text(text = "Name for joining")}, isError = isNameEmpty, supportingText = {if (isNameEmpty){
-                Text(text = "Darf nicht leer sein")
-            } })
+            TextField(
+                modifier = modifier,
+                value = joinLink,
+                onValueChange = { joinScreenViewModel.updateJoinLink(it) },
+                label = { Text(text = "Link for joining") },
+                isError = isLinkEmpty,
+                supportingText = {
+                    if (isLinkEmpty) {
+                        Text(text = "Darf nicht leer sein")
+                    }
+                })
+            TextField(
+                modifier = modifier,
+                value = joinName,
+                onValueChange = { joinScreenViewModel.updateJoinName(it) },
+                label = { Text(text = "Name for joining") },
+                isError = isNameEmpty,
+                supportingText = {
+                    if (isNameEmpty) {
+                        Text(text = "Darf nicht leer sein")
+                    }
+                })
             Button(modifier = modifier, onClick = {
-                if(joinLink.equals("")){
+                if (joinLink.equals("")) {
                     isLinkEmpty = true
-                }else if(joinName.equals("")){
+                } else if (joinName.equals("")) {
                     isNameEmpty = true
-                }else{
+                } else {
                     joinScreenViewModel.join { onNavigateToTrackingScreen() }
                 }
             }) {
@@ -66,12 +100,27 @@ fun JoinScreen(modifier: Modifier = Modifier, onNavigateToInitialScreen: () -> U
             }
             Column {
                 Text(text = "Verlauf")
-                lastCollections.reversed().forEach{
+                lastCollections.reversed().forEach {
                     ListItem(
-                        headlineContent = { Text(text = it)},
-                        modifier=Modifier.clickable { joinScreenViewModel.updateJoinLink(it) })
+                        headlineContent = { Text(text = it) },
+                        modifier = Modifier.clickable { joinScreenViewModel.updateJoinLink(it) })
                 }
             }
         }
     }
+}
+
+@Composable
+fun ConnectionErrorDialog(onDecline: () -> Unit) {
+    AlertDialog(onDismissRequest = { onDecline() }, confirmButton = { TextButton(onClick = { onDecline() }) {
+        Text(text = "OK")
+    }}, icon = {
+        Icon(
+            Icons.Default.Info,
+            contentDescription = "error while trying to connect to server"
+        )
+    }, title = { Text(text = "Verbindungsfehler") },
+        text = {
+            Text(text = "Es konnte der Sammlung nicht beigetreten werden.")
+        })
 }
