b459833af74bac3d498a1068fdfa38be5c5f4dad
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-05-14 20:48:07 +0200

    added error handling for empty input fields

--- frontend-android/app/src/main/java/de/flyndre/flat/composables/presetscreen/PresetScreen.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/composables/presetscreen/PresetScreen.kt
@@ -19,6 +19,9 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,44 +38,97 @@ import de.flyndre.flat.database.AppDatabase
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PresetScreen(
-    modifier: Modifier = Modifier, navController: NavController, topBarText: String, onNavigateToCreateGroupScreen: () -> Unit, onNavigateToTrackingScreen: () -> Unit, presetScreenViewModel: PresetScreenViewModel){
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    topBarText: String,
+    onNavigateToCreateGroupScreen: () -> Unit,
+    onNavigateToTrackingScreen: () -> Unit,
+    presetScreenViewModel: PresetScreenViewModel,
+) {
     val presetName by presetScreenViewModel.presetName.collectAsState()
     val presetDescription by presetScreenViewModel.presetDescription.collectAsState()
-
+    var isNameEmpty by remember { mutableStateOf(false) }
+    var isDescriptionEmpty by remember { mutableStateOf(false) }
 
     Scaffold(topBar = {
-        CenterAlignedTopAppBar(title = { Text(text = topBarText) }, navigationIcon = { IconButton(
-            onClick = { onNavigateToCreateGroupScreen() }) {
-            Icon(
-                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                contentDescription = "back to create group screen"
-            )
-        }})
+        CenterAlignedTopAppBar(title = { Text(text = topBarText) }, navigationIcon = {
+            IconButton(
+                onClick = { onNavigateToCreateGroupScreen() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "back to create group screen"
+                )
+            }
+        })
     },
         bottomBar = {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally)
             ) {
-                Button(onClick = { presetScreenViewModel.openCollection(onNavigateToTrackingScreen)  }) {
+                Button(onClick = {
+                    if (presetName.equals("")) {
+                        isNameEmpty = true
+                    } else if (presetDescription.equals("")) {
+                        isDescriptionEmpty = true
+                    } else {
+                        presetScreenViewModel.openCollection(onNavigateToTrackingScreen)
+                    }
+                }) {
                     Text("Save and Start")
                 }
-                Button(onClick = { presetScreenViewModel.savePresetToDatabase(); onNavigateToCreateGroupScreen() }) {
+                Button(onClick = {
+                    if (presetName.equals("")) {
+                        isNameEmpty = true
+                    } else if (presetDescription.equals("")) {
+                        isDescriptionEmpty = true
+                    } else {
+                        presetScreenViewModel.savePresetToDatabase()
+                        onNavigateToCreateGroupScreen()
+                    }
+                }) {
                     Text("Save")
                 }
             }
-        }) {
-        innerPadding ->
-        Column (modifier = Modifier.padding(innerPadding)) {
+        }) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
             val modifier = Modifier.padding(10.dp)
-            TextField(modifier = modifier, value = presetName, onValueChange = {presetScreenViewModel.updatePresetName(it)}, label = {Text(text = "Preset Name")})
-            TextField(modifier = modifier, value = presetDescription, onValueChange = {presetScreenViewModel.updatePresetDescription(it)}, label = {Text(text = "Preset Description")})
-            Card (modifier = modifier){
-                GoogleMap(onMapClick = { navController.navigate("collectionarea") },
+            TextField(
+                modifier = modifier,
+                value = presetName,
+                onValueChange = { presetScreenViewModel.updatePresetName(it) },
+                label = { Text(text = "Preset Name") },
+                isError = isNameEmpty,
+                supportingText = {
+                    if (isNameEmpty) {
+                        Text(text = "Darf nicht leer sein")
+                    }
+                })
+            TextField(
+                modifier = modifier,
+                value = presetDescription,
+                onValueChange = { presetScreenViewModel.updatePresetDescription(it) },
+                label = { Text(text = "Preset Description") },
+                isError = isDescriptionEmpty,
+                supportingText = {
+                    if (isDescriptionEmpty) {
+                        Text(text = "Beschreibung darf nicht leer sein")
+                    }
+                })
+            Card(modifier = modifier) {
+                GoogleMap(
+                    onMapClick = { navController.navigate("collectionarea") },
                     cameraPositionState = CameraPositionState(position = presetScreenViewModel.getCameraPosition()),
-                    uiSettings = MapUiSettings(zoomControlsEnabled = false, zoomGesturesEnabled = false, scrollGesturesEnabled = false, rotationGesturesEnabled = false, tiltGesturesEnabled = false)){
-                    if(presetScreenViewModel.getCollectionArea().isNotEmpty()){
-                        presetScreenViewModel.getCollectionArea().forEach{ area ->
+                    uiSettings = MapUiSettings(
+                        zoomControlsEnabled = false,
+                        zoomGesturesEnabled = false,
+                        scrollGesturesEnabled = false,
+                        rotationGesturesEnabled = false,
+                        tiltGesturesEnabled = false
+                    )
+                ) {
+                    if (presetScreenViewModel.getCollectionArea().isNotEmpty()) {
+                        presetScreenViewModel.getCollectionArea().forEach { area ->
                             Polygon(
                                 points = area.listAreaPoints,
                                 fillColor = area.color.copy(alpha = 0.5f),
