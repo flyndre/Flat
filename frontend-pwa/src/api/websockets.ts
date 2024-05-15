2e891a68cf6cfe40ca3ba9e4853e475b234d2808
Ruben <ruben.elias.kraft@outlook.de>
2024-05-15 09:04:04 +0200

    refactor: refactoring

--- frontend-pwa/src/api/websockets.ts
+++ frontend-pwa/src/api/websockets.ts
@@ -1,131 +1,157 @@
-import { member } from "../types/member";
-import { TrackingLog } from '@/types/TrackingLog';
-import { useObservable } from '@vueuse/rxjs';
-import { WebSocketStatus, useIntervalFn } from '@vueuse/core';
-import { SERVER_UPDATE_INTERVAL } from '@/data/constants';
+import { clientId } from '@/data/clientMetadata';
+import { ParticipantTrack } from '@/types/ParticipantTrack';
+import { useIntervalFn, useWebSocket } from '@vueuse/core';
 import { LineString } from 'geojson';
-import { liveQuery } from 'dexie';
+import { ref, watch } from 'vue';
 import db from '../data/db';
-import { useWebSocket } from '@vueuse/core'
-import { Ref, ref, watch } from "vue";
-import { mdiConsoleLine } from "@mdi/js";
-import { clientId } from "@/data/clientMetadata";
-import { ParticipantTrack } from "@/types/ParticipantTrack";
 
-
- 
-export let members = ref([{id: clientId.value, name: "admin", color: "#fffff", progress : []}] as ParticipantTrack[]);
+export let members = ref([
+    { id: clientId.value, name: 'admin', color: '#fffff', progress: [] },
+] as ParticipantTrack[]);
 export let newInvite = ref([]);
 export let isAdmin = true;
 let latestSendTimestamp = null;
 
-export function setAdmin(boo){
-    isAdmin = boo
+export function setAdmin(boo) {
+    isAdmin = boo;
 }
 
-const { status, data, send, open, close } = useWebSocket('wss://flat.buhss.de/api/ws', {
-    autoReconnect: true
-  })
+const { status, data, send, open, close } = useWebSocket(
+    'wss://flat.buhss.de/api/ws',
+    {
+        autoReconnect: true,
+    }
+);
 const {
     isActive,
     pause: pauseInterval,
     resume: resumeInterval,
-} = useIntervalFn(
-    () => {
+} = useIntervalFn(() => {
+    console.log('Sending Trackingpoints...');
+    var lineStringOfPosition = {
+        type: 'LineString',
+        coordinates: [
+            [48.38685, 8.58066],
+            [48.387614, 8.581562],
+            [48.387386, 8.577933],
+        ],
+    } as LineString;
 
-        console.log("Sending Trackingpoints...")
-        var lineStringOfPosition = {type: "LineString", coordinates: [[48.386850,8.580660], [48.387614, 8.581562], [48.387386, 8.577933]]} as LineString
-        
-        var newlatest = null;
-        db.trackingLogs.toCollection().each(el => {(el.timestamp > latestSendTimestamp) || latestSendTimestamp == null ? lineStringOfPosition.coordinates.push(el.position) : null; newlatest = el.timestamp})
-    
-        latestSendTimestamp = newlatest;
+    var newlatest = null;
+    db.trackingLogs.toCollection().each((el) => {
+        el.timestamp > latestSendTimestamp || latestSendTimestamp == null
+            ? lineStringOfPosition.coordinates.push(el.position)
+            : null;
+        newlatest = el.timestamp;
+    });
 
-        send(JSON.stringify({type: 1, trackId: "94042b6e-a317-499a-af3d-1d32e58cbbb2", track: lineStringOfPosition, clientId: clientId.value}))
-    },
-    7000
-);
+    latestSendTimestamp = newlatest;
 
-watch(data, data => {
-    console.log("New Websocket Message:")
-    console.log(data)
-    if(JSON.parse(data).type === "AccessRequest"){
-        newInvite.value.push(JSON.parse(data))
+    send(
+        JSON.stringify({
+            type: 1,
+            trackId: '94042b6e-a317-499a-af3d-1d32e58cbbb2',
+            track: lineStringOfPosition,
+            clientId: clientId.value,
+        })
+    );
+}, 7000);
+
+watch(data, (data) => {
+    let websocketMessage = JSON.parse(data);
+    switch (websocketMessage.type) {
+        case 'AccessRequest':
+            if (websocketMessage.type === 'AccessRequest') {
+                newInvite.value.push(JSON.parse(data));
+            }
+            break;
+        case 'CollectionUpdate':
+            websocketMessage.collection.confirmedUsers.forEach((element) => {
+                if (
+                    members.value.filter((el) => el.id === element.clientId)
+                        .length === 0
+                ) {
+                    members.value.push({
+                        name: element.username,
+                        id: element.clientId,
+                        color: '#fffff',
+                        progress: [],
+                    });
+                }
+            });
+            break;
+        case 'IncrementalTrack':
+            let memberOfTrack = members.value.filter(
+                (el) => el.id === websocketMessage.clientId
+            )[0];
+            let listOfTracks = memberOfTrack.progress.filter(
+                (el) => el.id === websocketMessage.trackId
+            );
+
+            if (listOfTracks.length == 0) {
+                memberOfTrack.progress.push({
+                    id: websocketMessage.trackId,
+                    track: websocketMessage.track,
+                });
+            } else {
+                listOfTracks[0].track.coordinates.push.apply(
+                    listOfTracks[0].track.coordinates,
+                    websocketMessage.track.coordinates
+                );
+            }
+
+            memberOfTrack.progress.push.apply(
+                memberOfTrack.progress,
+                websocketMessage.track
+            );
+            break;
     }
-    if(JSON.parse(data).type === "CollectionUpdate"){
-        console.log(JSON.parse(data).collection.confirmedUsers)
-        JSON.parse(data).collection.confirmedUsers.forEach(element => {
-            console.log(element); 
-        if((members.value.filter(el => el.id === element.clientId)).length === 0){
-            console.log("adding to Members:")
-           members.value.push({name: element.username, id: element.clientId, color: "#fffff", progress: [] })
-        }
-        });
-        console.log(members)
-    }
-    if(JSON.parse(data).type === "IncrementalTrack"){
-        console.log(members.value); 
-        console.log(JSON.parse(data).clientId == members.value[0].id); 
-        let memberOfTrack = (members.value.filter(el => el.id === JSON.parse(data).clientId))[0]
+});
 
-        let listOfTracks = memberOfTrack.progress.filter(el => el.id === JSON.parse(data).trackId)
-
-        if(listOfTracks.length == 0){
-            memberOfTrack.progress.push({id: JSON.parse(data).trackId, track: JSON.parse(data).track})
-        }else{
-            listOfTracks[0].track.coordinates.push.apply(listOfTracks[0].track.coordinates, JSON.parse(data).track.coordinates)
-        }
-
-        memberOfTrack.progress.push.apply(memberOfTrack.progress, JSON.parse(data).track)
-    }
-  
-})
-
-export function closeCollection(collectionId : string){
-    const answer = {type:  3, collectionId : collectionId}
-    send(JSON.stringify(answer))
+export function closeCollection(collectionId: string) {
+    const answer = { type: 3, collectionId: collectionId };
+    send(JSON.stringify(answer));
 }
 
-export function acceptOrDeclineAccessRequest(choice : boolean, username : string, clientId : string, collectionId : string){
+export function acceptOrDeclineAccessRequest(
+    choice: boolean,
+    username: string,
+    clientId: string,
+    collectionId: string
+) {
     const answer = {
-        type:  "AccessRequest",
+        type: 'AccessRequest',
         collectionId: collectionId,
         clientId: clientId,
         username: username,
-        accepted: choice
-    }
+        accepted: choice,
+    };
+
+    send(JSON.stringify(answer));
+}
+
+export function establishWebsocket(clientId: string, collectionId: string) {
+    const answer = {
+        type: 0,
+        clientId: clientId,
+        collectionId: collectionId,
+    };
 
     send(JSON.stringify(answer));
 
-}
+    console.log('Established Websocket.');
 
-export function establishWebsocket(clientId : string, collectionId : string){
-    const answer = {
-        type: 0, 
-        clientId: clientId,
-        collectionId: collectionId
-    }
-
-    send(JSON.stringify(answer))
-
-    console.log("Established Websocket.")
-    
-    resumeInterval(); 
-    
-    
-}
-
-export function pauseSendingToServer(){
-    pauseInterval();
-}
-
-export function resumeSendingToServer(){
     resumeInterval();
 }
 
-export function acceptInvite(uuid: string, username: string){
-    console.log(members);
+export function pauseSendingToServer() {
+    pauseInterval();
 }
 
+export function resumeSendingToServer() {
+    resumeInterval();
+}
 
-
+export function acceptInvite(uuid: string, username: string) {
+    console.log(members);
+}
