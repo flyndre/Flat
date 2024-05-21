f903e129e08d0c5f7d6481ebebe85f3ffa24fb13
dasdawidt <David_Maier@gmx.de>
2024-05-21 21:20:39 +0200

    refactor: translations in json files

--- frontend-pwa/src/plugins/I18nPlugin.ts
+++ frontend-pwa/src/plugins/I18nPlugin.ts
@@ -1,50 +1,11 @@
+import de from '@/assets/lang/de.json';
+import en from '@/assets/lang/en.json';
 import { createI18n } from 'vue-i18n';
 
 const messages = {
-    en: {
-        prompts: {
-            back: 'Back',
-            cancel: 'Cancel',
-            close: 'Close',
-            confirm: 'Yes',
-            deny: 'No',
-            settings: 'Settings',
-            dark_theme: 'Dark',
-            light_theme: 'Light',
-            device_theme: 'Device',
-            persistence_enabled: 'Save settings',
-            persistence_disabled: "Don't save settings",
-            copy_url_success: 'URL copied!',
-            copy_text_success: 'Text copied!',
-        },
-        locales: {
-            en: 'English',
-            de: 'German',
-        },
-    },
-    de: {
-        prompts: {
-            back: 'Zurück',
-            cancel: 'Abbrechen',
-            close: 'Schließen',
-            confirm: 'Ja',
-            deny: 'Nein',
-            settings: 'Einstellungen',
-            dark_theme: 'Dunkel',
-            light_theme: 'Hell',
-            device_theme: 'Gerät',
-            persistence_enabled: 'Einstellungen speichern',
-            persistence_disabled: 'Einstellungen nicht speichern',
-            copy_url_success: 'URL kopiert!',
-            copy_text_success: 'Text kopiert!',
-        },
-        locales: {
-            en: 'Englisch',
-            de: 'Deutsch',
-        },
-    },
+    en,
+    de,
 };
-
 const availableLocales = Object.keys(messages).map((l) => l.toLowerCase());
 const locale = navigator.languages.find((l) =>
     availableLocales.includes(l.toLowerCase())
