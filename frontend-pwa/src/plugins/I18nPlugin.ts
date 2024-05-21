5fb10e0a7d5d749db5ac8395796cf89e21e8480b
dasdawidt <David_Maier@gmx.de>
2024-05-21 23:27:38 +0200

    feat: add german translation

--- frontend-pwa/src/plugins/I18nPlugin.ts
+++ frontend-pwa/src/plugins/I18nPlugin.ts
@@ -2,15 +2,16 @@ import de from '@/assets/lang/de.json';
 import en from '@/assets/lang/en.json';
 import { createI18n } from 'vue-i18n';
 
+const fallbackLocale = 'en';
 const messages = {
     en,
     de,
 };
 const availableLocales = Object.keys(messages).map((l) => l.toLowerCase());
-const locale = navigator.languages.find((l) =>
-    availableLocales.includes(l.toLowerCase())
-);
-const fallbackLocale = 'en';
+const locale =
+    navigator.languages.find((l) =>
+        availableLocales.includes(l.toLowerCase())
+    ) ?? fallbackLocale;
 
 const I18nPlugin = createI18n({
     availableLocales,
