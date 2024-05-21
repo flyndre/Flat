ef99f38fec493b1eac396e963a0aa2184e0685b7
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-05-21 14:32:35 +0200

    changed color values

--- frontend-android/app/src/main/java/de/flyndre/flat/ui/theme/Theme.kt
+++ frontend-android/app/src/main/java/de/flyndre/flat/ui/theme/Theme.kt
@@ -10,21 +10,22 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80
+        primary = Color(0xFF9FF6),
+        secondary = Color(0xFFA6BA),
+        tertiary = Color(0xc698FF)
 )
 
 private val LightColorScheme = lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40
+        primary = Color(0xFF9FF6),
+        secondary = Color(0xc698FF),
+        tertiary = Color(0xFFA6BA)
 
         /* Other default colors to override
     background = Color(0xFFFFFBFE),
