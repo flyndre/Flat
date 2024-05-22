19b61a3d033462303773f6f81e175cf837358852
dasdawidt <David_Maier@gmx.de>
2024-05-22 02:04:35 +0200

    feat: goofy ahh spanish translation

--- frontend-pwa/src/plugins/I18nPlugin.ts
+++ frontend-pwa/src/plugins/I18nPlugin.ts
@@ -1,11 +1,13 @@
 import de from '@/assets/lang/de.json';
 import en from '@/assets/lang/en.json';
+import es from '@/assets/lang/es.json';
 import { createI18n } from 'vue-i18n';
 
 const fallbackLocale = 'en';
 const messages = {
     en,
     de,
+    es,
 };
 const availableLocales = Object.keys(messages).map((l) => l.toLowerCase());
 const locale =
