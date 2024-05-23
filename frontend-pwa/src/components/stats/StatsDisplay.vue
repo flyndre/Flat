7341e99aac8a0e5cd23b40b29705e07f16198b76
dasdawidt <David_Maier@gmx.de>
2024-05-23 11:25:22 +0200

    feat: add division and participant stats

--- frontend-pwa/src/components/stats/StatsDisplay.vue
+++ frontend-pwa/src/components/stats/StatsDisplay.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import { CollectionStats } from '@/types/stats/CollectionStats';
-import { computed } from 'vue';
-import MapWithControls from '../map/MapWithControls.vue';
 import { formatDateRange, getDuration } from '@/util/datetimeUtils';
-import { useI18n } from 'vue-i18n';
+import { getParticipantColor } from '@/util/trackingUtils';
+import {
+    mdiAccountCircle,
+    mdiCalendar,
+    mdiTextureBox,
+    mdiTimerOutline,
+} from '@mdi/js';
 import Panel from 'primevue/panel';
-import { mdiCalendar, mdiTextureBox, mdiTimer, mdiTimerOutline } from '@mdi/js';
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
 import MdiTextButtonIcon from '../icons/MdiTextButtonIcon.vue';
+import MapWithControls from '../map/MapWithControls.vue';
 
 const props = defineProps<{
     stats?: CollectionStats;
@@ -18,10 +24,10 @@ const dateRange = computed(() =>
     formatDateRange(props.stats.startDate, props.stats.finishDate, locale.value)
 );
 
-const properties = computed<
+const generalStats = computed<
     {
-        messageCode: string;
         icon: string;
+        messageCode: string;
         value: string;
     }[]
 >(() => [
@@ -38,9 +44,45 @@ const properties = computed<
     {
         icon: mdiTextureBox,
         messageCode: 'components.stats_display.area_total',
-        value: `${props.stats.converedArea} m²`,
+        value: `ca. ${props.stats.converedArea} m²`,
     },
 ]);
+
+const divisionStats = computed<
+    {
+        id: string;
+        icon: string;
+        label: string;
+        color: string;
+        value: string;
+    }[]
+>(() =>
+    props.stats.divisionStats.map((d) => ({
+        id: d.id,
+        icon: mdiTextureBox,
+        label: d.name,
+        color: d.color,
+        value: `ca. ${d.coveredArea.toFixed(2)} m²`,
+    }))
+);
+
+const participantStats = computed<
+    {
+        id: string;
+        icon: string;
+        label: string;
+        color: string;
+        value: string;
+    }[]
+>(() =>
+    props.stats.participantStats.map((p) => ({
+        id: p.id,
+        icon: mdiAccountCircle,
+        label: p.name,
+        color: p.color ?? getParticipantColor(p.id, props.stats.divisionStats),
+        value: `ca. ${p.coveredDistance.toFixed(2)} m`,
+    }))
+);
 </script>
 
 <template>
@@ -67,15 +109,61 @@ const properties = computed<
                     class="grid grid-cols-[minmax(20%,_max-content)_minmax(50%,_1fr)] gap-3"
                 >
                     <div
-                        v-for="stat of properties"
+                        v-for="stat of generalStats"
                         :key="stat.messageCode"
                         class="grid grid-cols-subgrid col-span-2"
                     >
                         <span
                             class="text-ellipsis overflow-hidden whitespace-nowrap"
                         >
-                            <MdiTextButtonIcon :icon="stat.icon" />
+                            <MdiTextButtonIcon class="ml-1" :icon="stat.icon" />
                             {{ $t(stat.messageCode) }}
+                        </span>
+                        <span>{{ stat.value }}</span>
+                    </div>
+                </div>
+            </Panel>
+            <Panel :header="$t('components.stats_display.title_divisions')">
+                <div
+                    class="grid grid-cols-[minmax(20%,_max-content)_minmax(50%,_1fr)] gap-3"
+                >
+                    <div
+                        v-for="stat of divisionStats"
+                        :key="stat.label"
+                        class="grid grid-cols-subgrid col-span-2"
+                    >
+                        <span
+                            class="text-ellipsis overflow-hidden whitespace-nowrap"
+                        >
+                            <MdiTextButtonIcon
+                                class="ml-1"
+                                :style="{ color: stat.color }"
+                                :icon="stat.icon"
+                            />
+                            {{ stat.label }}
+                        </span>
+                        <span>{{ stat.value }}</span>
+                    </div>
+                </div>
+            </Panel>
+            <Panel :header="$t('components.stats_display.title_participants')">
+                <div
+                    class="grid grid-cols-[minmax(20%,_max-content)_minmax(50%,_1fr)] gap-3"
+                >
+                    <div
+                        v-for="stat of participantStats"
+                        :key="stat.label"
+                        class="grid grid-cols-subgrid col-span-2"
+                    >
+                        <span
+                            class="text-ellipsis overflow-hidden whitespace-nowrap"
+                        >
+                            <MdiTextButtonIcon
+                                class="ml-1"
+                                :style="{ color: stat.color }"
+                                :icon="stat.icon"
+                            />
+                            {{ stat.label }}
                         </span>
                         <span>{{ stat.value }}</span>
                     </div>
