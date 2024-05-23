ed6ec24ded4bef65cf61a120dbcae4b80852c05f
dasdawidt <David_Maier@gmx.de>
2024-05-23 21:02:17 +0200

    fix: log position each second

--- frontend-pwa/src/data/constants.ts
+++ frontend-pwa/src/data/constants.ts
@@ -1,4 +1,4 @@
-export const TRACKING_INTERVAL = 3000;
+export const TRACKING_INTERVAL = 1000;
 export const SERVER_UPDATE_INTERVAL = 6000;
 export const TOAST_LIFE = 3000;
 export const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
