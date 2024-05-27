52dd06738babc76bab73af44fe35ed8cd53ad0dc
dasdawidt <David_Maier@gmx.de>
2024-05-27 21:25:48 +0200

    fix: add delay for reconnection of ws

--- frontend-pwa/src/data/constants.ts
+++ frontend-pwa/src/data/constants.ts
@@ -1,6 +1,7 @@
 export const TRACKING_INTERVAL = 1000;
 export const SERVER_UPDATE_INTERVAL = 6000;
 export const TOAST_LIFE = 3000;
+export const WS_RECONNECT_DELAY = 500;
 export const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 export const UNASSIGNED_PARTICIPANT_COLOR = '#64748b';
 export const INACTIVE_PARTICIPANT_COLOR = '#cdcdcd';
